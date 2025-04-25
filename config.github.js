7ddd79a5c74eba8f99458e289deaea7e8fb638dc
Josh Tubbs <jtubbs@Joshs-MacBook-Pro-3.local>
2025-04-25 13:00:17 -0400

    feat: add GitHub Pages specific configuration

---
+++ config.github.js
@@ -0,0 +1,10 @@
+// Firebase configuration for GitHub Pages
+const firebaseConfig = {
+    apiKey: "AIzaSyA51Q5E-FO0x92q4ZRySyPuY2kGF4LFBQs",
+    authDomain: "jt-pomodoro-with-userlogins.firebaseapp.com",
+    projectId: "jt-pomodoro-with-userlogins",
+    storageBucket: "jt-pomodoro-with-userlogins.firebasestorage.app",
+    messagingSenderId: "986429345281",
+    appId: "1:986429345281:web:91bf52d8fd3acd5a313a00",
+    measurementId: "G-QPGT32MHGG"
+}; 
